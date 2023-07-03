300001f5474ac7e9816c712787b9911e81a9715c
Kirill Grinkov <79636456163@yandex.ru>
2023-07-03 23:02:43 +0400

    addtask41

---
+++ task41/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+
+Console.Write("Какое количество чисел вы понируете ввести? Введите ответ: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[]Array = new int[num];
+
+FillArray(num);
+Console.Write($"\n Чисел больше нуля: {Amount(Array)}   ");
+
+
+
+void FillArray(int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        Console.WriteLine($"Введите {i+1} элемент массива");
+        Array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+int Amount(int[]Array)
+{
+    int i = 0;
+    int count = 0;
+    while (i < Array.Length)
+    {
+        if(Array[i]>0) count++; i++;
+    }
+    return count;
+}
+
+
